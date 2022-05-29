32af1799222ad444d1c2e53e505787dc622ed2af
MortySanchez <tobias.knobloch@steinlaender.de>
2022-05-29 17:10:06 +0200

    Code revised

--- AoC1/AoC1_2.ps1
+++ AoC1/AoC1_2.ps1
@@ -2,44 +2,44 @@
     Title:     Advent of Code
     Challenge: Day 1 Part 2 
     Status:    Solved!
+    Solution:  1858
 #>
 
-$Content = Get-Content -Path 'C:\Users\Tobias\Documents\AdventOfCode\AoC1\AoC1_2.txt'
+$fileContent = Get-Content -Path "$PSScriptRoot\Challenge_Part_1_Data.txt";
 
 
 [int]$increasements = 0;
 [int]$decreasements = 0;
 
-for ( [int]$i = 0; $i -lt $Content.Length; $i++ ) {
+for ( [int]$i = 0; $i -lt $fileContent.Length; $i++ ) {
   
-    $nums_A = $Content[$i..($i+2)]
-    $nums_B = $Content[($i+1)..($i+3)]
+    $nums_A = $fileContent[$i..($i+2)];
+    $nums_B = $fileContent[($i+1)..($i+3)];
 
     if ( $nums_A.Length -eq 3 -and $nums_B.Length -eq 3 ) {
-        
-        
-            # sum_A und sum_B bilden
+    
+        foreach ( $num in $nums_A ) {
+            [int]$sum_A += [int]$num;
+        }
 
-            foreach ( $num in $nums_A ) {
-                [int]$sum_A += [int]$num
-            }
-
-            foreach ( $num in $nums_B ) {
-                [int]$sum_B += [int]$num
-            }
+        foreach ( $num in $nums_B ) {
+            [int]$sum_B += [int]$num;
+        }
         
 
         if ( $sum_B -gt $sum_A ) {
             $increasements++;
-            Write-Host "$sum_B -gt $sum_A"
+            "{0} -gt {1}" -f $sum_B, $sum_A;
+
         }
         else {
             $decreasements++;
-            Write-Host "$sum_B -lt $sum_A"
+            "{0} -lt {1}" -f $sum_B, $sum_A;
         }
     }
     Clear-Variable sum_A
     Clear-Variable sum_B
 }
-Write-Host "Increasements: $increasements"
-Write-Host "Decreasements: $decreasements"
+
+"Increasements: {0}" -f $increasements;
+"Decreasements: {0}" -f $decreasements;
