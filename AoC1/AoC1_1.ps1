8cfed521d52e081fb24994d80d23a32b8ad4a8ae
MortySanchez <tobias.knobloch@steinlaender.de>
2022-05-29 21:24:39 +0200

    Removed unnecessary lines

--- AoC1/AoC1_1.ps1
+++ AoC1/AoC1_1.ps1
@@ -1,21 +1,17 @@
 ﻿<#
     Title:     Advent of Code
     Challenge: Day 1 Part 1 
-    Status:    Solved! --> Not anymore, something changed and we get a wrong value!
+    Status:    Solved! --> Not anymore, something changed and we get a wrong value of 1830!
     Solution:  1832
 #>
 
 $fileContent = Get-Content -Path "$PSScriptRoot\Puzzle_Input.txt";
 [int]$increasements = 0;
 
-for($i = 1; $i -lt $fileContent.Length; $i++) {
-    
+# Skip first line of fileContent to get first increasement
+for([int]$i = 1; $i -le $fileContent.Length; $i++) {
 
-    $tmp_a = $fileContent[$i];
-    $tmp_b = $fileContent[$i-1];
-
-
-    if ($tmp_a -gt $tmp_b) {
+    if($fileContent[$i] -gt $fileContent[$i-1]) {
         $increasements++;
     }
 }
