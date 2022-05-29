a72605f59537d7a1d736f30c6ebecde4e8930979
MortySanchez <tobias.knobloch@steinlaender.de>
2022-05-29 21:30:53 +0200

    Code adjustment, clean code

--- AoC2/AoC2_1.ps1
+++ AoC2/AoC2_1.ps1
@@ -4,18 +4,17 @@
     Status:    Solved!
 #>
 
-$path = 'C:\Users\Tobias\Documents\AdventOfCode\AoC2\AoC2.txt'
-$content = Get-Content -Path $path
+$fileContent = Get-Content -Path "$PSScriptRoot\Puzzle_Input.txt"
 
 [int]$horizontal_position = 0;
 [int]$depth = 0;
 
-$content | ForEach-Object {
+$fileContent | ForEach-Object {
 
-    $move_direction = $_.Split(' ')[0]
-    $move_value =     $_.Split(' ')[1]
+    $move_direction = $_.Split(' ')[0];
+    $move_value =     $_.Split(' ')[1];
 
-    switch ($move_direction)
+    switch($move_direction)
     {
         'forward' {
             $horizontal_position += [int]$move_value;
@@ -29,5 +28,4 @@ $content | ForEach-Object {
     }
 }
 
-$solution = $horizontal_position * $depth
-$solution
+"Solution: {0}" -f ($horizontal_position * $depth);
